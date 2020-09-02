e8d854af460f8301b83fc89c801c724ef8841145
Sarah Lange <sarah.lange@openknowledge.de>
2020-09-02 10:42:02 +0200

    update pact

--- src/test/java/de/openknowledge/sample/address/BillingAddressServiceTest.java
+++ src/test/java/de/openknowledge/sample/address/BillingAddressServiceTest.java
@@ -15,60 +15,43 @@
  */
 package de.openknowledge.sample.address;
 
-import static java.lang.Integer.valueOf;
-import static java.lang.System.getProperty;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import org.apache.meecrowave.Meecrowave;
 
-import java.net.URI;
+import org.apache.meecrowave.junit5.MeecrowaveConfig;
+import org.apache.meecrowave.testing.ConfigurationInject;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.junit.ArquillianTest;
-import org.jboss.arquillian.junit.ArquillianTestClass;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import au.com.dius.pact.provider.junit.PactRunner;
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
-import au.com.dius.pact.provider.junit.target.HttpTarget;
-import au.com.dius.pact.provider.junit.target.Target;
-import au.com.dius.pact.provider.junit.target.TestTarget;
-import de.openknowledge.sample.address.application.AddressApplication;
-import de.openknowledge.sample.address.domain.Address;
-
-@RunAsClient
-@RunWith(PactRunner.class)
 @Provider("billing-service")
 @PactFolder("src/test/pacts")
+@MeecrowaveConfig
 public class BillingAddressServiceTest {
 
-    @ClassRule
-    public static ArquillianTestClass arquillianTestClass = new ArquillianTestClass();
-    @Rule
-    public ArquillianTest arquillianTest = new ArquillianTest();
-    @TestTarget
-    public final Target target = new HttpTarget("http", "localhost", valueOf(getProperty("test.http.port", "6001")), "/billing");
-    @ArquillianResource
-    private URI uri;
+    @ConfigurationInject
+    private Meecrowave.Builder config;
 
-    @Deployment
-    public static WebArchive createDeployment() throws Exception {
-        PomEquippedResolveStage pomFile = Maven.resolver().loadPomFromFile("pom.xml");
-        return ShrinkWrap.create(WebArchive.class, "billing.war")
-                .addPackage(AddressApplication.class.getPackage())
-                .addPackage(Address.class.getPackage())
-                .addAsLibrary(pomFile.resolve("org.apache.commons:commons-lang3").withTransitivity().asSingleFile());
+
+    @BeforeEach
+    public void setUp(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", config.getHttpPort(), "/"));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
     }
 
     @State("Three customers")
-    public void setThreeCustomers() {
-        // nothing to do
+    public void toDefaultState() {
+        System.out.println("Now service in default state");
     }
 }
